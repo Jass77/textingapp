fcc2cf02e1ea5821c7776ff7e8acf7ee7792f67e
EC2 Default User <ec2-user@ip-172-31-84-11.ec2.internal>
2020-07-26 07:09:02 +0000

    Updates

---
+++ precache-manifest.8eaf2449407d26b2d51cd368ac85ed67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9cdfc6f869dd43ccbb03cd9e921777",
+    "url": "/textingapp/index.html"
+  },
+  {
+    "revision": "94c34fef891b8ca155be",
+    "url": "/textingapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1248b10d2dac852f10f2",
+    "url": "/textingapp/static/js/2.085106d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/textingapp/static/js/2.085106d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c34fef891b8ca155be",
+    "url": "/textingapp/static/js/main.9c6eabd3.chunk.js"
+  },
+  {
+    "revision": "55543b804e18dd6cb73e",
+    "url": "/textingapp/static/js/runtime-main.6e4320e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/textingapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
